09aab6b4690055598173578e35e1c25a5fc0eee9
Ceraph1216 <MaverickHunterDN@gmail.com>
2021-09-20 17:38:28 -0400

    - replaced text social links with logos

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,10 @@
 import Link from 'next/link';
 import React from 'react';
+import { useThemeStore } from '~/stores/useThemeStore';
 
 export function Footer() {
+  const { theme } = useThemeStore();
+
   return (
     <>
       <footer className="pt-4 pb-8 xl:pt-8 bg-footerBG">
@@ -49,21 +52,39 @@ export function Footer() {
             <li className="w-1/2 md:w-1/3 lg:w-1/3">
               <div className="text-left lg:text-center">
                 <h2 className="text-accentText text-2xl mb-4 pt-8 lg:pt-0">Community</h2>
-                <ul>
+                <ul className="flex flex-row lg:flex-col items-center">
                   <li className="mb-4 hover:text-accentText transition-colors duration-200">
-                    <Link href="/">
-                      <a>Twitter</a>
-                    </Link>
+                    <a href="https://www.twitter.com">
+                      <img
+                        width={32}
+                        height={26}
+                        className="ml-2 mr-2"
+                        alt="Twitter"
+                        src={`/img/SocialTwitterIcon_${theme}.png`}
+                      />
+                    </a>
                   </li>
                   <li className="mb-4 hover:text-accentText transition-colors duration-200">
-                    <Link href="/">
-                      <a>Facebook</a>
-                    </Link>
+                    <a href="https://www.facebook.com">
+                      <img
+                        width={20}
+                        height={37}
+                        className="ml-2 mr-2"
+                        alt="Facebook"
+                        src={`/img/SocialFacebookIcon_${theme}.png`}
+                      />
+                    </a>
                   </li>
                   <li className="mb-4 hover:text-accentText transition-colors duration-200">
-                    <Link href="/">
-                      <a>Medium</a>
-                    </Link>
+                    <a href="https://www.medium.com">
+                      <img
+                        width={32}
+                        height={26}
+                        className="ml-2 mr-2"
+                        alt="Medium"
+                        src={`/img/SocialMediumIcon_${theme}.png`}
+                      />
+                    </a>
                   </li>
                 </ul>
               </div>
