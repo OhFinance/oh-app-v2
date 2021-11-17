e7d361dd202408a071e73f81723194655e080cc0
apedeveloper <71899046+apedeveloper@users.noreply.github.com>
2021-11-17 12:06:27 +0100

    [feat] - add web3 react provider and manager from Uniswap standard

---
+++ type.d.ts
@@ -0,0 +1,11 @@
+declare module 'fortmatic';
+
+interface Window {
+  ethereum?: {
+    isMetaMask?: true;
+    on?: (...args: any[]) => void;
+    removeListener?: (...args: any[]) => void;
+    autoRefreshOnNetworkChange?: boolean;
+  };
+  web3?: Record<string, unknown>;
+}
