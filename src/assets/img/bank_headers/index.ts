aed0c4fce411247aaa82d03e22dd9d78d3fd47df
Odysseus The King <info@liquid.software>
2022-08-01 12:57:32 -0700

    Add Avalanche USDC and USDT Banks

--- src/assets/img/bank_headers/index.ts
+++ src/assets/img/bank_headers/index.ts
@@ -1,3 +1,4 @@
 export { default as Aave } from './aave.jpg';
 export { default as Hummus } from './hummus.svg';
 export { default as Moonwell } from './moonwell.jpg';
+export { default as Yeti } from './yeti.png';
